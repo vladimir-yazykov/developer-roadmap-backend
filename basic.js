c071ffff37371aa28488cbf5ca2943a5cc6f3e9a
Vladimir Yazykov <neizerth@gmail.com>
2025-09-01 13:45:17 +0300

    two way data process

--- basic.js
+++ basic.js
@@ -1,14 +1,26 @@
 import express from "express";
 import roadmap from "./data/roadmap.js";
 import cors from "cors";
+import { v4 } from "uuid";
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
   res.send(roadmap);
+});
+
+app.post("/", (req, res) => {
+  roadmap.push({
+    id: v4(),
+    ...req.body,
+  });
+  res.send({
+    succes: true,
+  });
 });
 
 app.listen(port, () => {
